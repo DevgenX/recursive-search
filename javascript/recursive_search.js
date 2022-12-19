ac31bf251011866a86a76e7bd53754a31612a716
devgenx <sebastian110420@gmail.com>
2022-12-19 08:33:28 -0500

    finished

--- javascript/recursive_search.js
+++ javascript/recursive_search.js
@@ -1,5 +1,21 @@
+const { LoaderTargetPlugin } = require("webpack");
+
 function recursiveSearch(arr, target) {
   // type your code here
+
+  // if arr[0] is equals to target, return true;
+  // type your code here
+
+  // if the length of array is 0 and target is not hit, return false;
+
+  if (arr[0] === target) return true;
+
+  if (arr.length === 0) return false;
+
+  // while it is not equal, slice the array at 0
+
+  // then recurse
+  return recursiveSearch(arr.slice(1), target);
 }
 
 if (require.main === module) {
